498a606513d9106d8e1f91e5b240f04384ad45b3
Gratz Julia <gratzjulia@gmail.com>
2025-07-11 16:36:55 -0300

    change random.seed for Generator

--- functions.py
+++ functions.py
@@ -1,36 +1,36 @@
 import pandas as pd
 import numpy as np
 
-def generateDataset():
-    np.random.seed(42)
+def generateClassificationDataset():
+    generator = np.random.default_rng(42)
     num_samples = 3333
 
     # account_length: Int, Min 1/Max 243, próximo de uma distribuição uniforme ou normal truncada
-    account_length = np.random.randint(1, 244, num_samples)
+    account_length = generator.integers(1, 244, num_samples)
 
     # total_day_charge: Float, aproximar com distribuição normal truncada
-    total_day_charge = np.random.normal(loc=30.562, scale=9.259, size=num_samples)
+    total_day_charge = generator.normal(loc=30.562, scale=9.259, size=num_samples)
     total_day_charge = np.clip(total_day_charge, 0.000, 59.640).round(2) # Garante min e max
 
     # total_eve_charge: Float
-    total_eve_charge = np.random.normal(loc=17.084, scale=4.311, size=num_samples)
+    total_eve_charge = generator.normal(loc=17.084, scale=4.311, size=num_samples)
     total_eve_charge = np.clip(total_eve_charge, 0.000, 30.910).round(2)
 
     # total_night_charge: Float
-    total_night_charge = np.random.normal(loc=9.039, scale=2.276, size=num_samples)
+    total_night_charge = generator.normal(loc=9.039, scale=2.276, size=num_samples)
     total_night_charge = np.clip(total_night_charge, 1.040, 17.770).round(2)
 
     # total_intl_charge: Float
-    total_intl_charge = np.random.normal(loc=2.765, scale=0.754, size=num_samples)
+    total_intl_charge = generator.normal(loc=2.765, scale=0.754, size=num_samples)
     total_intl_charge = np.clip(total_intl_charge, 0.000, 5.400).round(2)
 
     # customer_service_calls: Int, min/max
     # Usar random.randint e clip para forçar a distribuição de inteiros
-    customer_service_calls = np.random.randint(0, 10, num_samples) # Min 0, Max 9
+    customer_service_calls = generator.integers(0, 10, num_samples) # Min 0, Max 9
 
     # churn: Binário (0 ou 1) com base na média (proporção de 1s)
     churn_rate = 0.145
-    churn = np.random.choice([0, 1], size=num_samples, p=[1 - churn_rate, churn_rate])
+    churn = generator.choice([0, 1], size=num_samples, p=[1 - churn_rate, churn_rate])
 
     churn_df = pd.DataFrame({
         'account_length': account_length,
